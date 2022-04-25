8eb1db42a9a12684c1b96d77e9605a0735ac1e11
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-25 15:46:09 +0200

    initial commit

---
+++ src/main/scala/DemoKafkaStreams.scala
@@ -0,0 +1,19 @@
+import java.util.Properties
+
+object DemoKafkaStreams extends App {
+
+  import org.apache.kafka.common.serialization.Serdes
+  import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig}
+
+  val props = new Properties()
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "demo-ks")
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass)
+  props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass)
+
+  val builder = new StreamsBuilder
+  builder.stream[String, String]("topic-demo-ks").mapValues((value: String) => String.valueOf(value.length)).to("new-demo-topic")
+
+  val streams = new KafkaStreams(builder.build, props)
+  streams.start()
+}
